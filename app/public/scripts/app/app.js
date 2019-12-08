b9303796a9b8cc8d749a3ae26cc8dcef7b5ad1ab
jorishr <joris@jorisr.com>
2019-12-08 04:59:25 +0100

    cookies bar styles and functionalityp; modal stylesa and functionality;

--- app/public/scripts/app/app.js
+++ app/public/scripts/app/app.js
@@ -1,6 +1,21 @@
-const   toggle     = document.querySelector('#toggle'),
-        extendMenu = document.querySelector('.menu__collapse');
+const   toggle        = document.querySelector('#toggle'),
+        extendMenu    = document.querySelector('.menu__collapse'),
+        modalClose    = document.querySelector('.modal button[type="submit"]'),
+        modal         = document.querySelector('.modal'),
+        cookieBar     = document.querySelector('.privacy-bar'),
+        cookieClosers = document.querySelectorAll('.privacy-bar a, .privacy-bar i');
+
+let closeBtns = Array.from(cookieClosers);
+closeBtns.forEach(btn => {
+    btn.addEventListener('click', function(e){
+        cookieBar.classList.remove('in-view');
+    });
+});
 
 toggle.addEventListener('click', function(e){   
     extendMenu.classList.toggle('visible');
-})
+});
+
+modalClose.addEventListener('click', function(e){   
+    modal.classList.remove('in-view');
+});
