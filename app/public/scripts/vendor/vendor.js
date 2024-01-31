acc609c3a3162c02287ab40019f6acf3ce28e054
jorishr <joris@liondigits.com>
2024-01-31 12:31:27 +0100

    [FEAT] Adds snowfall-js-plugin
    - Adds snowfall-js-plugin v0.0.6
    - Removes obselte code.

--- app/public/scripts/vendor/vendor.js
+++ app/public/scripts/vendor/vendor.js
@@ -1,5 +1,25 @@
 import initMap from "./maps.js";
-import runSnowfall from "./snowfall.js";
+import { snowAnimationStart } from "snowfall-js-plugin";
 
 initMap();
-runSnowfall();
+
+document.addEventListener("DOMContentLoaded", function () {
+  snowAnimationStart({
+    logLevel: "info",
+    snowfall: {
+      count: 33,
+      maxSpeed: 6,
+      canvasHeightLimit: 1,
+    },
+    switches: {
+      txt: "Sneeuw",
+      styles: {
+        bgClrOff: "#202020",
+        bgClrOn: "#7a2800",
+        toggleClr: "#ffffc0",
+        txtClr: "#ffffc0",
+        txtPosition: "1",
+      },
+    },
+  });
+});
